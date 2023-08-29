18a69d163a7dcf1f745811dbbedf0731b4a4eae4
Rayane Gaudêncio <rayanegaudencio11@gmail.com>
2023-08-29 10:25:02 -0300

    ME testes: calculadora desconto salário

---
+++ src/main/java/org/example/CalculadoraSalarioApp.java
@@ -0,0 +1,54 @@
+package org.example;
+
+public class CalculadoraSalarioApp {
+    final double SALARIO_PRIMEIRA_FAIXA = 1320;
+    final double SALARIO_SEGUNDA_FAIXA = 2751.29;
+    final double SALARIO_TERCEIRA_FAIXA = 3856.94;
+    final double SALARIO_QUARTA_FAIXA = 7507.49;
+
+    final double PORCENTAGEM_PRIMEIRA_FAIXA = 0.075;
+    final double PORCENTAGEM_SEGUNDA_FAIXA = 0.09;
+    final double PORCENTAGEM_TERCEIRA_FAIXA = 0.12;
+    final double PORCENTAGEM_QUARTA_FAIXA = 0.14;
+
+    final double MAXIMO_IMPOSTO_PRIMEIRA_FAIXA = 99;
+    final double MAXIMO_IMPOSTO_SEGUNDA_FAIXA = 112.61;
+    final double MAXIMO_IMPOSTO_TERCEIRA_FAIXA = 154.28;
+
+    final double MAXIMO_IMPOSTO = 876.97;
+
+
+    public double calcularINSS(double salarioBruto) {
+
+        if (salarioBruto <= SALARIO_PRIMEIRA_FAIXA) {
+            return calcPrimeiraFaixa(salarioBruto);
+        } else if (salarioBruto > SALARIO_PRIMEIRA_FAIXA && salarioBruto <= SALARIO_SEGUNDA_FAIXA) {
+            return calcSegundaFaixa(salarioBruto);
+        } else if (salarioBruto > SALARIO_SEGUNDA_FAIXA && salarioBruto <= SALARIO_TERCEIRA_FAIXA) {
+            return calcTerceiraFaixa(salarioBruto);
+        } else if (salarioBruto > SALARIO_TERCEIRA_FAIXA && salarioBruto <= SALARIO_QUARTA_FAIXA) {
+            return calcQuartaFaixa(salarioBruto);
+        } else {
+            return MAXIMO_IMPOSTO;
+        }
+    }
+
+    private double calcPrimeiraFaixa(double salarioBruto) {
+        return salarioBruto * PORCENTAGEM_PRIMEIRA_FAIXA;
+    }
+
+    private double calcSegundaFaixa(double salarioBruto) {
+        double calcSegundaFaixa = (salarioBruto - SALARIO_PRIMEIRA_FAIXA) * PORCENTAGEM_SEGUNDA_FAIXA;
+        return calcSegundaFaixa + MAXIMO_IMPOSTO_PRIMEIRA_FAIXA;
+    }
+
+    private double calcTerceiraFaixa(double salarioBruto) {
+        double calcTerceiraFaixa = (salarioBruto - SALARIO_SEGUNDA_FAIXA) * PORCENTAGEM_TERCEIRA_FAIXA;
+        return calcTerceiraFaixa + MAXIMO_IMPOSTO_PRIMEIRA_FAIXA + MAXIMO_IMPOSTO_SEGUNDA_FAIXA;
+    }
+
+    private double calcQuartaFaixa(double salarioBruto) {
+        double calcQuartaFaixa =  (salarioBruto - SALARIO_TERCEIRA_FAIXA) * PORCENTAGEM_QUARTA_FAIXA;
+        return calcQuartaFaixa + MAXIMO_IMPOSTO_PRIMEIRA_FAIXA + MAXIMO_IMPOSTO_SEGUNDA_FAIXA + MAXIMO_IMPOSTO_TERCEIRA_FAIXA;
+    }
+}
---
+++ src/test/java/org/example/CalculadoraSalarioAppTest.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculadoraSalarioAppTest {
+
+    CalculadoraSalarioApp calcSalario = new CalculadoraSalarioApp();
+
+    @DisplayName("Deveria retornar R$97,50")
+    @Test
+    void testeCalculoDescontoPrimeiraFaixa() {
+        assertEquals( 97.50, calcSalario.calcularINSS(1300),0.01);
+    }
+
+    @DisplayName("Deveria retornar R$115,20")
+    @Test
+    void testeCalculoDescontoSegundaFaixa() {
+        assertEquals(115.20, calcSalario.calcularINSS(1500), 0.01);
+    }
+
+    @DisplayName("Deveria retornar R$263,06")
+    @Test
+    void testeCalculoDescontoTerceiraFaixa() {
+        assertEquals(263.06, calcSalario.calcularINSS(2600), 0.01);
+    }
+
+    @DisplayName("Deveria retornar R$665,92")
+    @Test
+    void testeCalculoDescontoQuartaFaixa() {
+        assertEquals(665.92, calcSalario.calcularINSS(6000), 0.01);
+    }
+
+    @DisplayName("Deveria retornar R$876,97")
+    @Test
+    void testeCalculoDescontoAcimaQuartaFaixa() {
+        assertEquals(876.97, calcSalario.calcularINSS(10000), 0.01);
+    }
+}
